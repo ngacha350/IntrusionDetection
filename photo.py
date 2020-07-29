cfa5880743ae90b9c0f79b6a4cab3f770d0b4f7d
ngacha350 <ngacha350@gmail.com>
2020-07-29 10:52:22 +0300

    Add files via upload

--- photo.py
+++ photo.py
@@ -20,7 +20,6 @@ while True:
 		print("image captured")
 		time.sleep(1)
 	break
-system('convert -delay 10 -loop 0 /home/pi/Desktop/gearbox/New/image*.jpg /home/pi/Desktop/gearbox/timelapse.gif')
 system('ffmpeg -framerate 30 -pattern_type glob -i "/home/pi/Desktop/gearbox/New/image*.jpg" -s:v 1024x768 -c:v libx264 -crf 17 -pix_fmt yuv420p /home/pi/Desktop/gearbox/timelapse.mp4')
 print('done')
 
